2dbe4a66b46a852005336d04222ab1460c865b6b
henriquensco <henriquensco@gmail.com>
2023-05-24 17:26:41 -0300

    Add: update create task repository

--- app/Repositories/TaskRepository.php
+++ app/Repositories/TaskRepository.php
@@ -2,17 +2,21 @@
 
 namespace App\Repositories;
 
+use App\DTO\CreateTaskDTO;
 use App\Repositories\Interfaces\TaskRepositoryInterface;
+use App\Services\Task\CreateTaskService;
 
-class TaskRepository implements TaskRepositoryInterface {
+class TaskRepository implements TaskRepositoryInterface
+{
     public function listTasks()
     {
         return [];
     }
 
-    public function createTask()
+    public function createTask(CreateTaskDTO $params): CreateTaskDTO|array
     {
-        return [];
+        $createTask = new CreateTaskService($params);
+        return $createTask->execute();
     }
 
     public function updateTask()
