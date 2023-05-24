a2b4439c5f7ea47ab128b0ffe505aec9e3ab3c69
henriquensco <henriquensco@gmail.com>
2023-05-24 19:43:07 -0300

    Add: array to interface

--- app/Repositories/Interfaces/TaskRepositoryInterface.php
+++ app/Repositories/Interfaces/TaskRepositoryInterface.php
@@ -7,7 +7,7 @@ use App\DTO\UpdateTaskDTO;
 
 interface TaskRepositoryInterface
 {
-    public function listTasks();
+    public function listTasks(): array;
     public function createTask(CreateTaskDTO $params): CreateTaskDTO|array;
     public function updateTask(array $params, string $uuid): UpdateTaskDTO|array;
     public function deleteTask(string $uuid): array;
